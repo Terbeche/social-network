fe2a6ec3f3d9309e0b48a4660634f773e371c0b6
terbeche <termus96@gmail.com>
2022-12-24 09:04:24 +0100

    Setup group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,45 @@
 class GroupsController < ApplicationController
+
+    def index
+        @groups = Group.all
+    end
+
+
+  def show
+    @group = Group.find(params[:id])
+    @members = group.users.all
+  end
+
+  def new
+    @group = Group.new
+    respond_to do |format|
+      format.html { render :new, locals: { group: @group } }
+    end
+  end
+
+  def create
+    @group = current_user.groups.new(group_params)
+    if @recipe.save
+      redirect_to user_group_path(@group.user_id, @group.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @group = Group.find params[:id]
+    @group.destroy
+    redirect_to user_group_path(@group.user_id)
+  end
+
+
+
+  private
+
+  def group_params
+    params
+      .require(:group)
+      .permit(:name)
+      .merge(user_id: params[:user_id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# User.destroy_all
+# Group.destroy_all
+# Post.destroy_all
+# Comment.destroy_all
+
+@user1 = User.create(name:"user1", email:"user1@gmail.com", password:"123456", password_confirmation:"123456")
+@user2 = User.create(name:"user2", email:"user2@gmail.com", password:"123456", password_confirmation:"123456")
+@user3 = User.create(name:"user3", email:"user3@gmail.com", password:"123456", password_confirmation:"123456")
+@user4 = User.create(name:"user4", email:"user4@gmail.com", password:"123456", password_confirmation:"123456")
+p "Created #{User.count} users"
+
+@group1 = Group.create(name:"First group", user_id:@user1.id)
+@group2 = Group.create(name:"Second group", user_id:@user1.id)
+@group3 = Group.create(name:"Third group", user_id:@user2.id)
+@group4 = Group.create(name:"Fourth group", user_id:@user3.id)
+p "Created #{Group.count} groups"
+
+
+@post1 = Post.create(title:"First post", body:"First post content", user_id:@user1.id, group_id:@group1.id)
+@post2 = Post.create(title:"First post", body:"First post content", user_id:@user2.id, group_id:@group1.id)
+@post3 = Post.create(title:"First post", body:"First post content", user_id:@user3.id, group_id:@group1.id)
+@post4 = Post.create(title:"First post", body:"First post content", user_id:@user1.id, group_id:@group2.id)
+
+p "Created #{Post.count} posts"
