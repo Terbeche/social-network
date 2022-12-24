7008ac90714da09cf2af253846f93d0b584f785f
terbeche <termus96@gmail.com>
2022-12-24 07:15:25 +0100

    Generate necessary models

---
+++ db/migrate/20221224061306_create_groups.rb
@@ -0,0 +1,10 @@
+class CreateGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
