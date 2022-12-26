41516f1f395c9ba2bec39051a3e6dee69d83d449
terbeche <termus96@gmail.com>
2022-12-26 17:40:51 +0100

    Add basic theme picker using tailwind and stimulus

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -1,32 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
+
 export default class extends Controller {
 
     static targets = [ "mode"]
 
-    connect() { 
+     connect() { 
         console.log("Hello, theme!", this.element)    
     }
-    change_theme (e) {
-        const body = document.body
+
+    change_theme(){
+        console.log("change_theme")
+
         const element = this.modeTarget
         const text = element.innerHTML
+        const theme = localStorage.getItem("theme")
+        console.log(theme)
 
-    if (text == "Dark") {
-        element.innerHTML = "White"
-        body.classList.remove("bg-white")
-        body.classList.add("bg-black")
-        body.classList.remove("text-black")
-        body.classList.add("text-white")
-        
-    } else {
-        element.innerHTML = "Dark"
-        body.classList.remove("bg-black")
-        body.classList.add("bg-white")
-        body.classList.remove("text-white")
-        body.classList.add("text-black")
-        
-    }
+        if (theme == "dark") {
+            document.body.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+            element.innerHTML = "Dark"
+        }else {
+            document.body.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+            element.innerHTML = "White"
+            }
+            const new_theme = localStorage.getItem("theme")
+
+        console.log("change_theme end" + new_theme)
 
     }
+    
 }
+
+
